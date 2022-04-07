3d617fd56981b8e7bbe9ae1cba03a42a5a22e3be
alimuratkuslu <alimuratkuslu@gmail.com>
2022-04-08 00:51:02 +0300

    Created Constructer and Getters-Setters

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,55 @@
 
 public class Person {
-
 	
+	private String name;
+	private String vorname;
+	private int telefonNummer;
+	private int ausweisNummer;
+	private String email;
+	
+	public Person(String name, String vorname, int telefonNummer, int ausweisNummer, String email) {
+		super();
+		this.name = name;
+		this.vorname = vorname;
+		this.telefonNummer = telefonNummer;
+		this.ausweisNummer = ausweisNummer;
+		this.email = email;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getVorname() {
+		return vorname;
+	}
+	public void setVorname(String vorname) {
+		this.vorname = vorname;
+	}
+	
+	public int getTelefonNummer() {
+		return telefonNummer;
+	}
+	public void setTelefonNummer(int telefonNummer) {
+		this.telefonNummer = telefonNummer;
+	}
+	
+	public int getAusweisNummer() {
+		return ausweisNummer;
+	}
+	public void setAusweisNummer(int ausweisNummer) {
+		this.ausweisNummer = ausweisNummer;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
 	
 }
