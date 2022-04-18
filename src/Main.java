96c6c59b98437b6d820312bbdb5b8b435686f9d3
alimuratkuslu <alimuratkuslu@gmail.com>
2022-04-18 19:21:57 +0300

    Created Database in PgAdmin and Connected it in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String jdbcURL = "jdbc:postgresql://localhost:5431/Autohändler";
+		String username = "postgres";
+		String password = "0201ALis..";
+		
+		try {
+			Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+			System.out.println("Connected to Postgres Server");
+			
+			connection.close();
+			
+		} catch (SQLException e) {
+			System.out.println("Error connecting to Postgres Server");
+			e.printStackTrace();
+		}
+		
+		
 	}
 
 }
